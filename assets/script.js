f2c05a35ac3f1c9e54a3e6623352920409cc3efe
Samantha Wolff <jasonwolff@Samanthas-MacBook-Air.local>
2022-03-04 00:45:25 -0600

    add questions, answers, and correct answers

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,71 @@
 // Define all variables
 
 // Header variables
-var viewHighscores = $('#view-highscores');
-var timer = $('#timer');
+var viewHighscoresBtn = $('#view-highscores');
+var timer = $('#timer'); // setInterval countdown
+var timeLeft = 100;
 
 // Start quiz section variables
-var startQuiz = $('#start-quiz');
+var startQuizEl = $('#start-quiz'); // id for entire section
 var startBtn = $('#start-btn');
 
 // Questions section variables
-var questions = $('#questions');
-var question = $('#question');
-var choice1Btn = $('#choice1');
-var choice2Btn = $('#choice2');
-var choice3Btn = $('#choice3');
-var choice4Btn = $('#choice4');
-var correctWrong = $('#correct-wrong');
+var questionsEl = $('#questions'); // id for entire section
+var question = $('#question'); 
+var choice1Btn = $('#choice1'); // button
+var choice2Btn = $('#choice2'); // button
+var choice3Btn = $('#choice3'); // button
+var choice4Btn = $('#choice4'); // button
+var correctWrong = $('#correct-wrong'); 
 
 // Finish quiz section variables
-var finishQuiz = $('#finish-quiz');
+var finishQuizEl = $('#finish-quiz'); // id for entire section
 var finalScore = $('#final-score');
 var name = $('#input-name');
-var submitScore = $('#submit-score');
+var submitScoreBtn = $('#submit-score'); // button
 
 // Highscore section variables
-var highscores = $('#highscores');
+var highscoresEl = $('#highscores'); // id for entire section
 var highscoreList = $('#highscore-list');
-var goBack = $('#go-back');
-var clearHighscores = $('#clear-highscores');
+var goBackBtn = $('#go-back'); // button
+var clearHighscoresBtn = $('#clear-highscores'); //button
+
+// Questions, answers, and correct answers
+var questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: ["A. <scripting>", "B. <script>", "C. <js>", "D. <javascript>"],
+        correctAnswer: "B"
+    },
+
+    {
+        question: "How do you create a function in JavaScript?",
+        answers: ["A. function myFunction()", "B. function:myFunction()", "C. function = myFunction()", "D. function myFunction{}"],
+        correctAnswer: "A"
+    },
+
+    {
+        question: "How do you write an IF statement in JavaScript?",
+        answers: ["A. if i = 5", "B. if i == 5 then", "C. if (i==5)", "D. if i = 5 then"],
+        correctAnswer: "C"
+    },
+
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        answers: ["A. onchange", "B. onmouseclick", "C. onmouseover", "D. onclick"],
+        correctAnswer: "D"
+    },
+
+    {
+        question: "How do you declare a JavaScript variable?",
+        answers: ["A. v carName;", "B. variable carName;", "C. var carName;", "D. carName;"],
+        correctAnswer: "C"
+    },
+
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        answers: ["A. =", "B. x", "C. -", "D. *"],
+        correctAnswer: "A"
+    }
+];
 
