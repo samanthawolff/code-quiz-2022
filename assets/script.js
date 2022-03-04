30194a73ba7cb4dbffeda7b5ae926b2fce1c2784
Samantha Wolff <jasonwolff@Samanthas-MacBook-Air.local>
2022-03-03 23:38:56 -0600

    declare all id variables from html

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+// Define all variables
+
+// Header variables
+var viewHighscores = $('#view-highscores');
+var timer = $('#timer');
+
+// Start quiz section variables
+var startQuiz = $('#start-quiz');
+var startBtn = $('#start-btn');
+
+// Questions section variables
+var questions = $('#questions');
+var question = $('#question');
+var choice1Btn = $('#choice1');
+var choice2Btn = $('#choice2');
+var choice3Btn = $('#choice3');
+var choice4Btn = $('#choice4');
+var correctWrong = $('#correct-wrong');
+
+// Finish quiz section variables
+var finishQuiz = $('#finish-quiz');
+var finalScore = $('#final-score');
+var name = $('#input-name');
+var submitScore = $('#submit-score');
+
+// Highscore section variables
+var highscores = $('#highscores');
+var highscoreList = $('#highscore-list');
+var goBack = $('#go-back');
+var clearHighscores = $('#clear-highscores');
+
